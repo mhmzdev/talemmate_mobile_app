a236bae65cf69c2d8d4069695072f743efc43ee1
Muhammad Hamza <hamza.6.shakeel@gmail.com>
2026-05-16 12:21:45 +0500

    feat: Update analysis options, add Podfile, and configure fonts for new font families

---
+++ lib/gen/assets/fonts.gen.dart
@@ -0,0 +1,24 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+class FontFamily {
+  FontFamily._();
+
+  /// Font family: Fraunces
+  static const String fraunces = 'Fraunces';
+
+  /// Font family: Geist
+  static const String geist = 'Geist';
+
+  /// Font family: GeistMono
+  static const String geistMono = 'GeistMono';
+
+  /// Font family: NotoNastaliqUrdu
+  static const String notoNastaliqUrdu = 'NotoNastaliqUrdu';
+}
